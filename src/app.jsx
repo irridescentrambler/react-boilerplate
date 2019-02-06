90f558642c40eb1d7d4b02ed79798453d6b81694
irridescentrambler <mohadikar59@gmail.com>
2019-02-06 18:24:40 +0530

    Adds lint checker and fixes lint errors.

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 class App extends React.Component {
-  render(){
-    return(
-      <h1>Hello Tango Hell</h1>
-    );
-  }
+	render(){
+		return(
+			<h1>Hello Tango Hell</h1>
+		);
+	}
 }
 
 export default App;
